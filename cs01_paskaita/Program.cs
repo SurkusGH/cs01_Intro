1ba4034bae985b277b074d058e8b5b2b3291db75
Surkus <v.surkus@me.com>
2021-12-20 20:52:07 +0200

    Initial commit

---
+++ cs01_paskaita/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cs01_paskaita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("cs01_paskaita");
+        }
+    }
+}
+

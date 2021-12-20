dfc9303b7714fee5c9fd5d8cc90cbbc504b50136
Surkus <v.surkus@me.com>
2021-12-20 21:42:52 +0200

    Update Program.cs

--- cs01_paskaita/Program.cs
+++ cs01_paskaita/Program.cs
@@ -7,6 +7,19 @@ namespace cs01_paskaita
         static void Main(string[] args)
         {
             Console.WriteLine("cs01_paskaita");
+            Console.WriteLine($"4 + 5 = {4 + 5}");
+
+            //int age = 15;
+            //string myName = "Vilmantas";
+            Console.WriteLine("Stringas: Koks Jūsų vardas: ");
+            string myName = Console.ReadLine();
+
+            Console.WriteLine("Integeris: Koks Jūsų amžius: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.ForegroundColor = ConsoleColor.DarkRed; // <-- paspalvoju consolės tekstą
+
+            Console.WriteLine($"Mano vardas yra {myName} ir man yra {age} metų");
         }
     }
 }
